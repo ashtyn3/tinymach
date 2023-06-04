a8580999067721db33c55c2b414ac3db5dbf0565
ashtyn3 <ashtynmorel@gmail.com>
2023-06-04 18:42:36 +0000

    Jun 4, 2023, 2:42 PM

--- src/utils/utils.zig
+++ src/utils/utils.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const ops = @import("../instructions.zig");
+const mem = @import("../memory.zig");
 
 pub fn intBuffer(comptime T: type, dat: T) ![]u8 {
     const allocator = std.heap.page_allocator;
@@ -11,4 +13,18 @@ pub fn intBuffer(comptime T: type, dat: T) ![]u8 {
 
 pub fn bufferInt(comptime T: type, dat: []u8) !T {
     return std.mem.readIntSlice(T, dat, std.builtin.Endian.Little);
+}
+
+pub fn wrap(comptime e_T: ops.types, comptime T: type, comptime dat: T) mem.Wrap {
+    var wrapper: mem.Wrap = undefined;
+    wrapper.unwrap_type = e_T;
+
+    switch (e_T) {
+        .T_u8 => wrapper.data.T_u8 = dat,
+        .T_u16 => wrapper.data.T_u16 = dat,
+        .T_u32 => wrapper.data.T_u32 = dat,
+        .T_u64 => wrapper.data.T_u64 = dat,
+    }
+
+    return wrapper;
 }
