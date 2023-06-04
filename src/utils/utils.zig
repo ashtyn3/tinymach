bb10c98bb8c22d7dd49c4d2025fc94fff375c764
ashtyn3 <ashtynmorel@gmail.com>
2023-06-04 21:04:02 +0000

    Jun 4, 2023, 5:04 PM

--- src/utils/utils.zig
+++ src/utils/utils.zig
@@ -17,15 +17,14 @@ pub fn bufferInt(comptime T: type, dat: []u8) !T {
 
 pub fn wrap(comptime e_T: ops.types, comptime T: type, dat: T) ops.vmErrors!mem.Wrap {
     var wrapper: mem.Wrap = undefined;
-    var data: mem.WrapData = mem.WrapData{ .T_u64 = 0 };
     wrapper.unwrap_type = e_T;
 
     std.debug.print("{}", .{e_T});
     switch (e_T) {
-        .T_u8 => data.T_u8 = dat,
-        .T_u16 => data.T_u16 = dat,
-        .T_u32 => data.T_u32 = dat,
-        .T_u64 => data.T_u64 = dat,
+        .T_u8 => wrapper.data = mem.WrapData{ .T_u8 = dat },
+        .T_u16 => wrapper.data = mem.WrapData{ .T_u16 = dat },
+        .T_u32 => wrapper.data = mem.WrapData{ .T_u32 = dat },
+        .T_u64 => wrapper.data = mem.WrapData{ .T_u64 = dat },
         else => return ops.vmErrors.badData,
     }
 
