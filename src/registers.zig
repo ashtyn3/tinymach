50ae51f975ab80d4f97a395af2b02fdec3973e5a
ashtyn3 <ashtynmorel@gmail.com>
2023-06-04 20:49:50 +0000

    Jun 4, 2023, 4:49 PM

--- src/registers.zig
+++ src/registers.zig
@@ -1,5 +1,7 @@
 const vm = @import("vm.zig");
 const mem = @import("memory.zig");
+const ops = @import("instructions.zig");
+const utils = @import("utils/utils.zig");
 pub const regs = enum { ip, r1, r2, r3, r4, r5, r6, r7 };
 
 pub const Registers = struct {
@@ -8,6 +10,9 @@ pub const Registers = struct {
     // register_alloc: std.mem.Allocator,
     registers: [8]mem.Wrap,
 
+    pub fn init(self: *Self) void {
+        self.registers[0] = utils.wrap(ops.types.T_u64, u64, 0);
+    }
     pub fn set(self: *Self, idx: regs, dat: mem.Wrap) void {
         self.registers[@intCast(usize, @enumToInt(idx))] = dat;
     }
